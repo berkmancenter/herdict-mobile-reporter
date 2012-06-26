07a55c3132a998fd8873cb87d0a10925885f217f
Tomas Reimers <tomasreimers@gmail.com>
2012-06-26 15:58:32 -0400

    Added walkthrough and quick restyle, still have to flesh out style and fix walkthrough stutter

--- www/resources/js/engine.js
+++ www/resources/js/engine.js
@@ -1,3 +1,7 @@
+// *******************
+// *** reporter ******
+// *******************
+
 $(document).bind( "mobileinit", function() {
 	// allow cross site requests
     $.mobile.allowCrossDomainPages = true;
@@ -10,10 +14,10 @@ $(document).ready(function (){
 	$.ajax({
 		url: 'http://www.herdict.org/action/ajax/plugin/init-currentLocation/',
 		success: function (data, status, jqxhr){
-			//var locationData = $.parseJSON(jqxhr.responseText);
-			locationData = $.parseJSON('{"countryShort":"US","ipTo":2365063167,"countryLong":"United States","ipFrom":2364997632,"ispName":"HARVARD UNIVERSITY"}');
+			var clean = cleanJSON(jqxhr.responseText);
+			locationData = $.parseJSON(clean);
 			$("#ispField")[0].value = locationData.ispName;
-			loadOtherFields();
+			$("#report").one("pageinit", loadOtherFields);
 		}
 	});
 });
@@ -72,7 +76,7 @@ function loadOtherFields(){
 	}
 }
 
-var countriesLoaded = 0;
+var countriesLoaded = 1; // the default "select country"
 
 function populateFields(section, obj){
 	$("#"+section+"Field").append('<option value="' + obj.value + '">' + obj.label + '</option>');
@@ -82,18 +86,16 @@ function populateFields(section, obj){
 	if (obj.value == locationData.countryShort){
 		$("#countriesField")[0].selectedIndex = (countriesLoaded - 1);
 		$("#countriesField").selectmenu("refresh");
+		$("#countriesField").trigger("change");
 	}
 }
 
-fieldsLoaded = 0;
+var fieldsLoaded = 0;
 
-function loadedData(){
+function loadedData(sectionLoaded){
 	fieldsLoaded++;
 	if (fieldsLoaded == toLoad.length){
-		for (var i = 0; i < toLoad.length; i++){
-			$("#"+toLoad[i]+"Field").selectmenu("refresh");
-			$("#"+toLoad[i]+"Field").trigger("change");
-		}
+		resetAllFields();
 	}
 }
 
@@ -108,7 +110,7 @@ function connectToBackupDB(){
 function connectToQueue(){
 	var db = window.openDatabase("toSend", "1.0", "Herdict report queue", 102400);
 	db.transaction(function (t){
-		t.executeSql("CREATE TABLE IF NOT EXISTS toSendQueue (id INTEGER PRIMARY KEY AUTOINCREMENT, category varchar(100), country varchar(3), location varchar(100), interest varchar(100), reason varchar(100), isp varchar(255), url varchar(4000),  accessible boolean)",
+		t.executeSql("CREATE TABLE IF NOT EXISTS toSendQueue (id INTEGER PRIMARY KEY AUTOINCREMENT, category varchar(100), country varchar(3), location varchar(100), interest varchar(100), reason varchar(100), isp varchar(255), url varchar(4000),  accessible boolean, comment varchar(2000))",
 		[],
 		function (t, r){
 
@@ -128,13 +130,14 @@ function queueUp(accessibleBoolean){
 	var interest = $("#interestsField")[0].value;
 	var reason = $("#reasonsField")[0].value; 
 	var isp = $("#ispField")[0].value;
-	var url = $("#urlField")[0].value;
+	var url = prepareURL($("#urlField")[0].value);
 	var accessible = (accessibleBoolean ? 1 : 0);
+	var comment = $("#commentField")[0].value;
 	// store in db
 	var db = connectToQueue();
 	db.transaction(function (t){
-		t.executeSql("INSERT INTO toSendQueue (category, country, location, interest, reason, isp, url, accessible) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
-			[category, country, location, interest, reason, isp, url, accessible],
+		t.executeSql("INSERT INTO toSendQueue (category, country, location, interest, reason, isp, url, accessible, comment) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+			[category, country, location, interest, reason, isp, url, accessible, comment],
 			function (t, r){
 				// begin checking until I can dequeue
 				checkHerdict();
@@ -155,32 +158,65 @@ function resetAllFields(){
 	$("#reasonsField option:selected").removeAttr("selected");
 	$("#reasonsField").selectmenu("refresh");
 	$("#urlField")[0].value = "";
+	$("#commentField")[0].value = "";
 	$(".accessSubmit").removeClass("ui-btn-active");
 }
 function deQueue(){
-	// connect to db
 	var db = connectToQueue();
-	
 	db.transaction(function (t){
 		t.executeSql("SELECT * FROM toSendQueue", [], 
-			function (t, results){
-				r = results;
+			function (t, r){
 				// send data to server
 				var resultsLen = r.rows.length;
 				for (var i = 0; i < resultsLen; i++){
-					// TODO: Make API Call
-					var toAlert = "";
-					for (var column in r.rows.item(i)){
-						toAlert += (column + ": " + (r.rows.item(i))[column] + "\n");
-					}
-					//alert(toAlert);
-					t.executeSql("DELETE FROM toSendQueue WHERE id=?", [r.rows.item(i).id]);
+					// prep for api
+					// TODO: add report.sourceID
+					var sourceId = "1"
+					// TODO: Replace dev2 with www
+					var reportRequest = "http://dev2.herdict.org/action/ajax/plugin/report?" + (r.rows.item(i).accessible ? "siteAccessible" : "siteInaccessible") + "&report.url=" + encodeURIComponent(r.rows.item(i).url) + "&report.country.shortName=" + encodeURIComponent(r.rows.item(i).country) + "&report.ispName=" + encodeURIComponent(r.rows.item(i).isp) + "&report.location=" + encodeURIComponent(r.rows.item(i).location) + "&report.interest=" + encodeURIComponent(r.rows.item(i).interest) + "&report.reason=" + encodeURIComponent(r.rows.item(i).reason) + "&report.tag=" + encodeURIComponent(r.rows.item(i).category) + "&report.comments=" + encodeURIComponent(r.rows.item(i).comment) + "&defaultCountryCode=" + encodeURIComponent(locationData.countryShort) + "&defaultISPName=" + encodeURIComponent(locationData.ispName) + "&report.sourceId=" + sourceId + "&encoding=ROT13";
+					alert(reportRequest);
+					// report 
+					$.ajax({
+						url: reportRequest,
+						success: (function (idToRemove){
+							return(function (data, status, jqxhr){
+								var db = connectToQueue();
+								db.transaction(function (t){
+									t.executeSql("DELETE FROM toSendQueue WHERE id=?", [idToRemove]);
+								});
+							});
+						})(r.rows.item(i).id),
+						error: function(jqxhr, status, errThrown){
+							alert(errThrown);
+						}
+					});
 				}
-		});
-
+			}
+		);
 	});
 	loadRandomDomain();
 }
+
+function checkIfDequeue(){
+	var db = connectToQueue();
+	db.transaction(function (t){
+		t.executeSql("SELECT * FROM toSendQueue", [], 
+			function (t, r){
+				if (r.rows.length > 0){
+					// so there is data left to dequeue
+					checkHerdict();
+				}
+			}
+		);
+	});
+}
+
+$(document).ready(function (){
+	checkIfDequeue();
+});
+
+// ui 
+
 function checkHerdict(){
 	// check if site is accessible, if so, dequeue 
 	$.ajax({
@@ -235,6 +271,7 @@ function toggleRandom (){
 	if (!randomMode){
 		randomMode = true;
 		$("#randomButton").addClass("toggledOn");
+		loadRandomDomain();
 	}
 	else {
 		randomMode = false;
@@ -246,7 +283,75 @@ $(document).ready(function (){
 	$("#randomButton").on("click", toggleRandom);
 });
 
-// child browser
+function prepareURL(url){
+	// strip http
+	if (url.substr(0,7) == "http://"){
+		url = url.substr(7);
+	}
+	// strip any trailing path, query, or hash
+	var offendingCharacters = ["/", "?", "#"];
+	for (var i = 0; i < offendingCharacters.length; i++){
+		url = (url.split(offendingCharacters[i]))[0];
+	}
+	// rot13
+	var lowercase = "abcdefghijklmnopqrstuvwxyz";
+ 	url = url.toLowerCase();
+	var urlArray = url.split("");
+	var lettersToIterateThru = urlArray.length;
+	for (var i = 0; i < lettersToIterateThru; i++){
+		var currentPosition = lowercase.indexOf(urlArray[i]);
+		if (currentPosition >= 0){
+			currentPosition += 13;
+			currentPosition = currentPosition%26;
+			urlArray[i] = lowercase[currentPosition];
+		}
+	}
+	url = urlArray.join("");
+	// that's all folks
+	return url;
+}
+
+function cleanJSON(json){
+	var start = 0;
+	var end = json.length;
+	if (json.charAt(0) == "("){
+		start++;
+	}
+	if (json.charAt(end - 1) == ")"){
+		end--;
+	}
+	return json.slice(start, end);
+}
+
+// *******************
+// *** walkthrough ***
+// *******************
+
+Object.defineProperty(window, "skipWalkthrough", {
+	get : function(){
+        if (window.localStorage.getItem("skipWalkthrough") === null){
+        	window.localStorage.setItem("skipWalkthrough", "false");
+        }
+        return window.localStorage.getItem("skipWalkthrough");
+    },  
+    set : function(newValue){
+    	window.localStorage.setItem("skipWalkthrough", newValue);
+    },  
+    enumerable : true,  
+    configurable : true
+});
+
+$(document).ready(function (){
+	$("#walkthrough").on("pagebeforeshow", function (){
+		if (window.skipWalkthrough == "true"){
+			$.mobile.changePage($("#report"));
+		}
+	});
+});
+
+// *******************
+// *** child browser *
+// *******************
 
 var childbrowser;
  
@@ -269,3 +374,22 @@ function onLinkClick(){
     }  
 }
 */
+
+// *******************
+// *** viewer ********
+// *******************
+
+function checkLink(){
+	var currentURL = $("#urlCheckField")[0].value;
+	currentURL = prepareURL(currentURL);
+	$.ajax({
+		url: 'http://www.herdict.org/action/ajax/plugin/site/'+currentURL+'/'+locationData.countryShort+'/ROT13/',
+		success: function(data, status, jqxhr) {
+			var clean = cleanJSON(jqxhr.responseText);
+			var siteData = $.parseJSON(clean);
+			$("#herdometer").html(siteData.sheepColor);
+			$("#globalCount").html(siteData.globalInaccessibleCount);
+			$("#localCount").html(siteData.countryInaccessibleCount);
+		}
+	});
+}
